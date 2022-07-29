16a5807f9e7c19cbed81299fb2d223a6ab72217c
Mucahit Acikgoz <acikgozmucahit91@gmail.com>
2022-07-29 13:44:25 +0200

    US 11 updates

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         },
         features = "./src/test/resources/features",
         glue = {"stepdefinitions","hooks"},
-        tags = "@US_23",
+        tags = "@US_04",
         dryRun = false)
 public class Runner {
 }
